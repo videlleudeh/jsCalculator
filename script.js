f4a4df4cd5633fc2f0cb62ba96f5707829933423
videlleudeh <chizzyudeh03@gmail.com>
2022-06-21 13:35:48 +0100

    calculator complete

---
+++ script.js
@@ -0,0 +1,19 @@
+alert("This is a basic arithmetic operation calculator. Enjoy!")
+
+let x = parseFloat(prompt("Enter a number: "))
+let opr = prompt("Select '+', '-', '*' or '/': ")
+let y = parseFloat(prompt("Enter another number: "))
+
+if (opr === '+')
+    {result = x + y;}
+else if (opr === '-')
+    {result = x - y;}
+else if (opr === '*')
+    {result = x * y;}
+else if (opr === '/')
+    {result = x / y;}
+else {
+    alert("Invalid user input or operator. Please refresh the page and try again!")
+}
+
+window.confirm("The answer is " + result + '.')
